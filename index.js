770f8126a973774d463b9ee9d5ee9663b1fd295d
Markis Taylor <m@rkis.cc>
2019-03-25 12:32:48 -0400

    Update jsdoc type definitions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function run() {
   try {
     if (isExecuting()) {
       var params = getParams();
-      if (params.comparison && params.version) {
+      if (params && params.comparison && params.version) {
         process.exit(ifVersion(process.version, params.comparison, params.version) ? 0 : 1);
       } else {
         throw 'No parameters specified';
@@ -30,22 +30,28 @@ function printHelp() {
 
 /**
  * Is the script executing or is this being 'required' into another script
- * 
+ *
  * @returns {boolean}
  */
 function isExecuting() {
   var scriptName = '/index.js';
   var binName = '/if-ver';
   var script = process.argv[1];
-  return script && 
+  return script &&
          script.substr(script.length - scriptName.length) === scriptName ||
          script.substr(script.length - binName.length) === binName;
 }
 
 /**
+ * @typedef {Object} Params - If-Ver Parameters
+ * @property {string} comparison - Comparison operator - gt, eq, lt, etc.
+ * @property {string} version - Semantic version - 4, 8.5.0, etc.
+ */
+
+/**
  * Check and convert process.argv into valid parameters
- * 
- * @returns {Object}
+ *
+ * @returns {Params}
  */
 function getParams() {
   var args = process.argv;
@@ -64,11 +70,11 @@ function getParams() {
 }
 
 /**
- * Using the compariison operator, compare two versions
- * 
- * @param {string} comparison 
+ * Using the comparison operator, compare two versions
+ *
+ * @param {string} comparison
  * @param {string} versionA
- * @param {string} versionB 
+ * @param {string} versionB
  */
 function ifVersion(versionA, comparison, versionB) {
   var verA = getVersion(versionA);
@@ -96,18 +102,27 @@ function ifVersion(versionA, comparison, versionB) {
       return equalTo(verA, verB) || lessThan(verA, verB);
   }
 
-  throw 'Invalid comparison operator "' + comparison + '"'
+  throw 'Invalid comparison operator "' + comparison + '"';
 }
 
 /**
+ * @typedef {Object} SemVer - Semantic Version (https://semver.org)
+ * @property {number} major - version with incompatible api changes
+ * @property {number} minor - version with new functionality but backwards compatible
+ * @property {number} patch - version with backwards-compatible bug fixes
+ */
+
+/**
  * Given a semver string return an object with the major, minor and patch broken out
- * 
- * @param {string} version 
+ *
+ * @param {string} version
+ * @returns {SemVer}
  */
 function getVersion(version) {
   var versionRegex = /^v?([\d]+)\.?([\d]+)?\.?([\d]+)?/
   var matches = versionRegex.exec(version);
   if (matches && matches.length === 4) {
+    /** @type SemVer */
     var versionObj = {};
     versionObj.major = parseInt(matches[1], 10) || 0;
     versionObj.minor = parseInt(matches[2], 10)  || 0;
@@ -119,9 +134,9 @@ function getVersion(version) {
 
 /**
  * Is Version A equal to Version B
- * 
- * @param {Object} a 
- * @param {Object} b 
+ *
+ * @param {SemVer} a
+ * @param {SemVer} b
  */
 function equalTo(a, b) {
   return a.major === b.major && b.minor === b.minor && a.patch === b.patch;
@@ -129,25 +144,25 @@ function equalTo(a, b) {
 
 /**
  * Is Version A greater than Version B
- * 
- * @param {Object} a 
- * @param {Object} b 
+ *
+ * @param {SemVer} a
+ * @param {SemVer} b
  */
 function greaterThan(a, b) {
-  return a.major > b.major || 
-         (a.major === b.major && a.minor > b.minor) || 
+  return a.major > b.major ||
+         (a.major === b.major && a.minor > b.minor) ||
          (a.major === b.major && a.minor === b.minor && a.patch > b.patch);
 }
 
 /**
  * Is Version A less than Version B
- * 
- * @param {Object} a 
- * @param {Object} b 
+ *
+ * @param {SemVer} a
+ * @param {SemVer} b
  */
 function lessThan(a, b) {
-  return a.major < b.major || 
-         (a.major === b.major && a.minor < b.minor) || 
+  return a.major < b.major ||
+         (a.major === b.major && a.minor < b.minor) ||
          (a.major === b.major && a.minor === b.minor && a.patch < b.patch);
 }
 
